00f4ebc05dc50776c80a8632202567ba21965467
CleidiomarFerreira <cleidiomartrompete@gmail.com>
2025-04-28 18:11:54 +0000

    Carta 1 e 2 refeitas

--- xadrez.c
+++ xadrez.c
@@ -22,46 +22,46 @@ int main() {
     // Entrada de dados para a Carta 1
     printf("Cadastro da Carta 1:\n");
 
-    printf("Digite o estado (letra de A a H):\n ");
+    printf("Digite o estado (letra de A a H): ");
     scanf(" %c", &estado1);
 
-    printf("Digite o código da carta (ex: A01):\n ");
-    scanf("%s", &codigo1);
+    printf("Digite o código da carta (ex: A01): ");
+    scanf("%s", codigo1);
 
-    printf("Digite o nome da cidade:\n ");
-    scanf(" %[^\n]", &nomeCidade1); // permite ler nomes com espaços
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]", nomeCidade1); // permite ler nomes com espaços
 
-    printf("Digite a população:\n ");
+    printf("Digite a população: ");
     scanf("%d", &populacao1);
 
-    printf("Digite a área (em km²):\n ");
+    printf("Digite a área (em km²): ");
     scanf("%f", &area1);
 
-    printf("Digite o PIB (em bilhões de reais):\n ");
+    printf("Digite o PIB (em bilhões de reais): ");
     scanf("%f", &pib1);
 
-    printf("Digite o número de pontos turísticos:\n ");
+    printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
     // Entrada de dados para a Carta 2
     printf("\nCadastro da Carta 2:\n");
 
-    printf("Digite o estado (letra de A a H):\n ");
+    printf("Digite o estado (letra de A a H): ");
     scanf(" %c", &estado2);
 
-    printf("Digite o código da carta (ex: B02):\n ");
-    scanf("%s", &codigo2);
+    printf("Digite o código da carta (ex: B02): ");
+    scanf("%s", codigo2);
 
-    printf("Digite o nome da cidade:\n ");
-    scanf(" %[^\n]", &nomeCidade2); // permite ler nomes com espaços
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]", nomeCidade2); // permite ler nomes com espaços
 
-    printf("Digite a população:\n ");
+    printf("Digite a população: ");
     scanf("%d", &populacao2);
 
-    printf("Digite a área (em km²):\n ");
+    printf("Digite a área (em km²): ");
     scanf("%f", &area2);
 
-    printf("Digite o PIB (em bilhões de reais):\n ");
+    printf("Digite o PIB:(em bilhões de reais) ");
     scanf("%f", &pib2);
 
     printf("Digite o número de pontos turísticos:\n ");
@@ -74,7 +74,7 @@ int main() {
     printf("Nome da Cidade: %s\n", nomeCidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("PIB: %.2f bilhões de reais  \n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
     // Exibição dos dados da Carta 2
@@ -84,7 +84,7 @@ int main() {
     printf("Nome da Cidade: %s\n", nomeCidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("PIB: %.2f bilhões de reais \n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
 
     return 0;
